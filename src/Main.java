daca24f418b070cf771ec14e9acef7f0f029d5c7
EceYardim <eceozgelenyardim@gmail.com>
2022-10-12 15:51:26 +0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Değişkenleri oluşturalım
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+
+        // Scanner sınıfını tanumlayalım
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıdan değerleri alalım
+        System.out.print("Matematik Notunuz: ");
+        matematik = input.nextInt();
+        System.out.print("Fizik Notunuz: ");
+        fizik = input.nextInt();
+        System.out.print( "Kimya Notunuz: ");
+        kimya = input.nextInt();
+        System.out.print( "Türkçe Notunuz: ");
+        turkce = input.nextInt();
+        System.out.print( "Tarih Notunuz: ");
+        tarih = input.nextInt();
+        System.out.print( "Muzik Notunuz: ");
+        muzik = input.nextInt();
+
+        int toplam = (matematik + fizik + kimya + turkce + tarih + muzik);
+        double ort = toplam / 6.0;
+        System.out.println("Ortalamanız:" + ort);
+        // Sonucu Karşılaştırma yapalım
+        boolean kosul1 = ort > 60;
+
+        //Koşulu ekleyelim
+        String str = kosul1?"Sınıfı Geçti":"Sınıfta Kaldı";
+        System.out.println(str);
+
+
+
+
+
+
+
     }
 }
